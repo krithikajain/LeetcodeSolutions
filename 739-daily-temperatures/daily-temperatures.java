612fb904b25a090fce4afb4582c107cc6bdd34b5
Krithika Jain <124558745+krithikajain@users.noreply.github.com>
2025-06-17 20:04:50 -0700

    Time: 60 ms (80.53%) | Memory: 59.9 MB (76.10%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,16 +1,22 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        int[] res = new int[temperatures.length];
-        Stack<int[]> stack = new Stack<>(); //pair of array in a stack [temp,idx]
+//questions: are there negative temperatures?
 
+        //things to keep in mind:
+        //if there are no warmer day ahead res[idx]=0
+        // track the preivous day temp using a stack
+        int[] res = new int[temperatures.length];
+        Stack<int[]> stack = new Stack<>(); //stores the value as [temp,idx]
+    
         for(int i=0; i<temperatures.length; i++){
             int t=temperatures[i];
             while(!stack.isEmpty() && t>stack.peek()[0]){
                 int[] pair = stack.pop();
-                res[pair[1]]= i-pair[1];
-            }
-            stack.push(new int[]{t,i});
+                res[pair[1]]=i-pair[1];
+        }
+        stack.push(new int[]{t,i});
         }
         return res;
     }
 }
+    //stack [{73,0}]
