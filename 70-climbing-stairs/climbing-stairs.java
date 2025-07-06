18e620e2663e97f2bba13ef6fc6dca22aa7ee169
Krithika Jain <124558745+krithikajain@users.noreply.github.com>
2025-07-05 23:33:43 -0700

    Time: 0 ms (100.00%) | Memory: 40.9 MB (5.40%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,8 +1,8 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n<=1)
+        if(n<=1){
             return 1;
-        
+        }
         int[] dp = new int[n+1];
         dp[0]=1;
         dp[1]=1;
@@ -13,3 +13,8 @@ class Solution {
         return dp[n];
     }
 }
+//we knpw that
+//n=0 no steps already at the top -- 1
+//n=1 take 1 to top -- 1
+//n=2 take 1+1 or 2 
+//n=3 take 1+1+1 or 2+1 or 1+2
