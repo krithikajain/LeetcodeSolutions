64c3f707c6d15c061ffa0a65e69215ba13dfa192
Krithika Jain <124558745+krithikajain@users.noreply.github.com>
2025-07-08 00:07:50 -0700

    Time: 50 ms (78.44%) | Memory: 50.6 MB (62.75%) - LeetSync

---
+++ 1582-design-browser-history/design-browser-history.java
@@ -0,0 +1,50 @@
+class BrowserHistory {
+    
+    private Node current;
+    public BrowserHistory(String homepage) {
+        current = new Node(homepage);
+    }
+    
+    public void visit(String url) {
+        Node newNode = new Node(url);
+        current.next=null;
+        newNode.prev = current;
+        current.next = newNode;
+        current = newNode;
+    }
+    
+    public String back(int steps) {
+        while(steps>0 && current.prev!=null){
+            current = current.prev;
+            steps--;
+        }
+        return current.url;
+    }
+    
+    public String forward(int steps) {
+        while(steps>0 && current.next!=null){
+            current = current.next;
+            steps--;
+        }
+        return current.url;
+    }
+    private static class Node{
+        String url;
+        Node prev, next;
+        public Node(String url){
+            this.url = url;
+        }
+    }
+}
+
+//will be using doubly linked list - avoid resizing of the array 
+//O(1)
+
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
