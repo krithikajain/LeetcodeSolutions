add4c6005a6001cf546cc25edcae3ac1bea8faae
Krithika Jain <124558745+krithikajain@users.noreply.github.com>
2025-07-08 10:50:03 -0700

    Time: 6 ms (98.23%) | Memory: 48.1 MB (44.51%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,26 +1,16 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        //create a map
-    Map<String, List<String>> anagrams = new HashMap<>();
-        //loop through the array of strings
-        for(String str:strs){
-            //sort the strs[i], since strings are immutable i will convert them into mutable objects
-            char[] ana = str.toCharArray();
-            //sort the array
-            Arrays.sort(ana); //["a","b","t"] -> convert back to string object
-            String sorted = new String(ana);
-            //check if it does not contain in the map
-            if(!anagrams.containsKey(sorted)){
-                //then create a key sorted and add a new list
-                anagrams.put(sorted, new ArrayList<>());
+        Map<String, List<String>> group = new HashMap<>();
+        for(String str: strs){
+            char[] anagram = str.toCharArray();
+            Arrays.sort(anagram);
+            String sorted = new String(anagram);
+            if(!group.containsKey(sorted)){
+                group.put(sorted, new ArrayList<>());
             }
-            //if contains then just get the key and add the value
-            anagrams.get(sorted).add(str);
+            group.get(sorted).add(str);
         }
-        //return type is list of lists
-        return new ArrayList<>(anagrams.values());
-       
-        
+        return new ArrayList<>(group.values());
     }
 }
 
