3497a44fd286ea0af227c412cba1ac663b84273b
Krithika Jain <124558745+krithikajain@users.noreply.github.com>
2025-07-08 15:56:54 -0700

    Time: 10 ms (96.14%) | Memory: 45.4 MB (97.32%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,23 +1,48 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // counting the freq
-        HashMap<Integer, Integer> freqMap = new HashMap<>();
-        for(int num:nums){
-            freqMap.put(num,freqMap.getOrDefault(num,0)+1);
-        }
-        // convert into list 
-        List<int[]> arr = new ArrayList<>();
-        for(Map.Entry<Integer, Integer> entry: freqMap.entrySet()){
-            arr.add(new int[]{entry.getValue(),entry.getKey()});
-        }
-        //sort the arr in ascending order
-        arr.sort((a,b)->a[0]-b[0]);
 
-        //print the top k elements
-        int[] result = new int[k];
-        for(int i=0; i<k; i++){
-            result[i]=arr.get(arr.size()-1-i)[1];
+        //create a map
+        Map<Integer, Integer> freqmap = new HashMap<>();
+        for(int num:nums){
+            freqmap.put(num,freqmap.getOrDefault(num,0)+1);
         }
-        return result;
+
+        //create a bucket array whose index value is my freq
+        List<Integer>[] bucket = new ArrayList[nums.length+1];
+        //go through the map and add the values in the bucket
+        for(int key: freqmap.keySet()){
+            int freq = freqmap.get(key);
+            if(bucket[freq] == null){
+            bucket[freq] = new ArrayList<>(); //initializing the arraylist inside the bucket
+        }
+        bucket[freq].add(key);
+        }
+
+        //store the result in an array
+        int[] result = new int[k];
+        int index=0; //pointer in my result
+        for(int i=bucket.length-1; i>=0 && index<k; i--){
+            if(bucket[i]!=null){
+                for(int num: bucket[i]){
+                    result[index++] = num;
+                    if(index == k)  break;
+                }
+            }
+        }
+            return result;
+
     }
 }
+
+//clarifications
+//empty string --- x
+//contains [1] --- [1]
+//k range [1, length of nums] - [1,2,3,4] k=4=length of nums
+//can have neg
+
+//create a map<value, freq> and put the values
+//append the values in the list, sort it and get the last k elements in the list --- O(nlogk)
+
+//bucket sort
+//create bucket array where the index would be my freq --- O(n)
+//ex=[1,2,2,40] => 1,1  2,2  40,1 --> 1,{1,40} 2,{2}
