0cea9c3e520ee76b852ee9e854fd8e93e7896478
Krithika Jain <124558745+krithikajain@users.noreply.github.com>
2025-07-16 09:01:44 -0700

    Time: 2 ms (37.03%) | Memory: 44.4 MB (81.32%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,28 +1,38 @@
 public class Solution {
     List<List<Integer>> res;
     public List<List<Integer>> permute(int[] nums) {
-        res = new ArrayList<>();
-        backtrack(nums, 0);
-        return res;
+    res = new ArrayList<>();
+    backtracking(nums, res, new ArrayList<>());
+    return res;
     }
 
-    public void backtrack(int[] nums, int idx) {
-        if (idx == nums.length) {
-            List<Integer> perm = new ArrayList<>();
-            for (int num : nums) perm.add(num);
-            res.add(perm);
+    private void backtracking(int[] nums, List<List<Integer>> result, List<Integer> tempList){
+        //base condition
+        if(tempList.size() == nums.length){ //all the positions are filled
+            result.add(new ArrayList<>(tempList));
             return;
         }
-        for (int i = idx; i < nums.length; i++) {
-            swap(nums, idx, i);
-            backtrack(nums, idx + 1);
-            swap(nums, idx, i);
+        for(int num: nums){
+            //there shouldn't a num repeated at the same postion we could skip it
+            if(tempList.contains(num))  continue;
+
+            tempList.add(num); //add it to the list
+
+            backtracking(nums, result, tempList); //call for the next positions and return 
+            
+            tempList.remove(tempList.size()-1); //remove the elements to go back to the previous state
         }
     }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
 }
+
+//Clarifications
+//contains all distinct nums
+//can contain neg
+//ranges from 1 - 6
+//n=1 [1] -> [[1]]
+//n=2 (0,1) -> [[0,1], [1,0]]
+//n=3 (1,2,3) -> [[1,2,3], [1,3,2],
+//                 [2,1,3], [2,3,1],
+//                 [3,1,2], [3,2,1]]
+//each position there is no repition of a number
+// _ _ _ -> 3 X 2 X 1 = 6 -> n! 
