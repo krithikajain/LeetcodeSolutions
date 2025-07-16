28b8b8d249d1859a03c150cfe561aa305d690774
Krithika Jain <124558745+krithikajain@users.noreply.github.com>
2025-07-16 12:50:14 -0700

    Time: 2 ms (71.98%) | Memory: 44.7 MB (86.08%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -9,14 +9,15 @@ class Solution {
     }
     private void backtracking(int[] nums, List<List<Integer>> result, List<Integer> tempList, boolean[] used){
         //base case if it forms a permuted list and also check if it is a duplicate permute
-        if(tempList.size() == nums.length && !result.contains(tempList)){
+        if(tempList.size() == nums.length){
             result.add(new ArrayList<>(tempList));
             return;
         }
         for(int i=0; i<nums.length; i++){
             //skip the element if it is already added
             if(used[i] == true) continue; 
-
+            //skip duplicates since it's sorted they are grouped together and we check if the prev is not used indicating that we aren't chosing it to create duplication
+            if(i>0 && nums[i] == nums[i-1] && !used[i-1]) continue;
             used[i] = true; //mark as used
             tempList.add(nums[i]); //add to the potential list
 
@@ -29,5 +30,4 @@ class Solution {
         }
     }
 }
-
  //boolean array to keep track of the number used
