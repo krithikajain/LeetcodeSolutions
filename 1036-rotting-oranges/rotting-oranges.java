8909f4b8ffd30ee1262bb89c0b4a711682c2de3e
Krithika Jain <124558745+krithikajain@users.noreply.github.com>
2025-07-21 16:58:35 -0700

    Time: 2 ms (67.83%) | Memory: 42.4 MB (62.20%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,37 +1,37 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        Queue<int[]> q = new ArrayDeque<>();
+        Queue<int[]> rot = new ArrayDeque<>();
         int fresh = 0;
         int time = 0;
 
         for(int r=0; r<grid.length; r++){
             for(int c=0; c<grid[0].length; c++){
-                if(grid[r][c] == 1){
-                    fresh++;
-                }
-                if(grid[r][c] == 2){
-                    q.offer(new int[]{r,c});
+                if(grid[r][c] == 1) fresh++;
+                if(grid[r][c] == 2) {
+                    rot.offer(new int[]{r,c});
                 }
             }
         }
+
         int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
-        while(!q.isEmpty() && fresh>0){
-            int length = q.size();
+        while(!rot.isEmpty() && fresh>0){
+            int length = rot.size();
             for(int i=0; i<length; i++){
-            int[] cur = q.poll();
-            int r = cur[0];
-            int c = cur[1];
+                int[] cur = rot.poll();
+                int r = cur[0];
+                int c = cur[1];
 
-            for(int[] dir:directions){
-                int row = r + dir[0];
-                int col = c + dir[1];
+                for(int[] direction : directions){
+                    int row = r + direction[0];
+                    int col = c + direction[1];
 
-                if (row >= 0 && row < grid.length && col >= 0 && col < grid[0].length && grid[row][col] == 1){
-                    grid[row][col] = 2;
-                    q.offer(new int[]{row,col});
-                    fresh --;
+                    if(row>=0 && row<grid.length && col>=0 && col<grid[0].length && 
+                    grid[row][col] == 1 ){
+                        grid[row][col] = 2;
+                        rot.offer(new int[]{row,col});
+                        fresh--;
+                    }
                 }
-            }
             }
             time++;
         }
