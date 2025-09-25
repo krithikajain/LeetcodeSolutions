6f9e2c4570e16a4d1e11a40a33a33fdbfb800c8c
Krithika Jain <124558745+krithikajain@users.noreply.github.com>
2025-09-25 08:32:40 -0400

    Time: 0 ms (100.00%) | Memory: 41.8 MB (8.99%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -15,17 +15,24 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        //base condition for recursion
+        //dfs traversal 
+        //base condition
         if(root==null){
             return null;
         }
-        //traverse to the left tree and right tree
-        TreeNode leftTree = invertTree(root.left);
-        TreeNode rightTree = invertTree(root.right);
-        //create a temp node to hold the value of left subtree temperorily
+        //traverse to the left and right tree
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        //create a temp node for swaping
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
-         return root;
+
+        return root;
+
     }
 }
+
+//tc - O(n)
+//sc - O(h)
