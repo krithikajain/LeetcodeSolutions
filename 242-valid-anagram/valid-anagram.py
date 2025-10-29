18d84237755702739b5a8eb60b81c07c1951efc1
Krithika Jain <124558745+krithikajain@users.noreply.github.com>
2025-10-29 13:10:32 -0400

    Time: 7 ms (89.31%) | Memory: 18 MB (49.59%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,19 +1,20 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        
-        #clarification; all lowercase; empty string?
-        #edge case: s.length != t.length - false
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #check if lengths are equal to proceed
         if len(s) != len(t):
             return False
 
-        countS, countT = {},{}
-        for i in range(len(s)):
-            countS[s[i]] = countS.get(s[i],0) + 1
-            countT[t[i]] = countT.get(t[i],0) + 1
+        # initialise a dictionary
+        count = {}
+        #add char from s
+        for char in s:
+            count[char] = count.get(char,0) + 1
+        #remove char from s looking into t
+        for char in t:
+            if char not in count:
+                return False
+            count[char] -= 1
 
-        return countS == countT
+            if count[char] < 0: return False
+        
+        return True
